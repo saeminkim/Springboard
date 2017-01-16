b3a1499ee485afcb09a7f1e9b416ac3f8ba6b803
saeminkim <askim715@gmail.com>
2017-01-16 17:51:07 -0500

    tidyr version
    Implemented tidyr function separate() to parse out "Product code /
    number" field

--- Data Wrangling/Data Wrangling 1/Data Wrangling 1.R
+++ Data Wrangling/Data Wrangling 1/Data Wrangling 1.R
@@ -1,5 +1,5 @@
 library(dplyr)
-library(stringr) 
+library(tidyr) 
 
 ## load refine_orignial.csv into R
 mydf <- read.csv("C:/Users/Andrew/Desktop/refine_original.csv")
@@ -8,10 +8,7 @@ mydf <- read.csv("C:/Users/Andrew/Desktop/refine_original.csv")
 mydf[,1] <- tolower(mydf[,1])
 
 ## 2. Separate product code and number
-list<-str_split_fixed(mydf$Product.code...number, "-", n = 2) ## delimits on "-" into list vector
-mydf$product_code <- as.character(list[,1]) ## adds product_code field to mydf dataframe
-mydf$product_number <- as.numeric(list[,2]) ## adds product_number field to mydf dataframe
-mydf$Product.code...number <- NULL ## drops product field
+mydf <- separate(mydf, Product.code...number, c("product_code","product_number"), sep = "-") ## breaks out Product.code...number field using tidyr separate() function
 
 ## 3. Add product categories
 mydf<-mutate(mydf, product_category = if_else(product_code == "p", "Smartphone",if_else(product_code == "v", "TV",if_else(product_code == "x","Laptop",if_else(product_code == "q", "Tablet","Other"))))) ## creates product category field based on product code
@@ -20,14 +17,14 @@ mydf<-mutate(mydf, product_category = if_else(product_code == "p", "Smartphone",
 mydf <- mutate(mydf, full_address = paste(mydf$address, mydf$city, mydf$country, sep = ", ")) ## concactenates address, city and county fields into full_address field
 
 ## 5. Create dummy variables for company and product category
-mydf <- mutate(mydf, company_phillips = if_else(substr(mydf[,1],1,1)=="p",1,0))
-mydf <- mutate(mydf, company_akzo = if_else(substr(mydf[,1],1,1)=="a",1,0))
-mydf <- mutate(mydf, company_van_houten = if_else(substr(mydf[,1],1,1)=="v",1,0))
-mydf <- mutate(mydf, company_unilever = if_else(substr(mydf[,1],1,1)=="u",1,0))
-mydf <- mutate(mydf, product_smartphone = if_else(substr(mydf[,6],1,1)=="p",1,0))
-mydf <- mutate(mydf, product_tv = if_else(substr(mydf[,6],1,1)=="v",1,0))
-mydf <- mutate(mydf, product_laptop = if_else(substr(mydf[,6],1,1)=="x",1,0))
-mydf <- mutate(mydf, product_tablet = if_else(substr(mydf[,6],1,1)=="q",1,0))
+mydf <- mutate(mydf, company_phillips = if_else(substr(mydf$ï..company,1,1)=="p",1,0))
+mydf <- mutate(mydf, company_akzo = if_else(substr(mydf$ï..company,1,1)=="a",1,0))
+mydf <- mutate(mydf, company_van_houten = if_else(substr(mydf$ï..company,1,1)=="v",1,0))
+mydf <- mutate(mydf, company_unilever = if_else(substr(mydf$ï..company,1,1)=="u",1,0))
+mydf <- mutate(mydf, product_smartphone = if_else(substr(mydf$product_code,1,1)=="p",1,0))
+mydf <- mutate(mydf, product_tv = if_else(substr(mydf$product_code,1,1)=="v",1,0))
+mydf <- mutate(mydf, product_laptop = if_else(substr(mydf$product_code,1,1)=="x",1,0))
+mydf <- mutate(mydf, product_tablet = if_else(substr(mydf$product_code,1,1)=="q",1,0))
 
 ## export cleaned dataframe into new csv file
 write.csv(mydf,"refine_clean.csv")
